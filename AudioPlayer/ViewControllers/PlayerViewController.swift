8b527e10395abbe11cf1829878a3ad359d7ac22b
Mikhail <qr.stronger@gmail.com>
2019-10-12 17:52:41 +0300

    PlayerViewController released

--- AudioPlayer/ViewControllers/PlayerViewController.swift
+++ AudioPlayer/ViewControllers/PlayerViewController.swift
@@ -7,8 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
-//Тут будет плеер с треком, выбранным на TracklistViewController
 class PlayerViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var coverImage: UIVisualEffectView!
+    @IBOutlet var artistNameLabel: UILabel!
+    @IBOutlet var trackNameLabel: UILabel!
+    @IBOutlet var trackReleaseYearLabel: UILabel!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var slider: UISlider!
+    
+    // MARK: - Private Properties
+    private var audioPlayer: AVAudioPlayer?
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let path = Bundle.main.path(forResource: "Rockstar.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+        } catch {
+            print(error)
+        }
+        
+        slider.maximumValue = Float(audioPlayer!.duration)
+        
+        _ = Timer.scheduledTimer(timeInterval: 0.1,
+                                 target: self,
+                                 selector: #selector(updateSlider),
+                                 userInfo: nil,
+                                 repeats: true)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        audioPlayer?.stop()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func changeAudioTime() {
+        audioPlayer?.play()
+        audioPlayer?.currentTime = TimeInterval(slider.value)
+        audioPlayer?.prepareToPlay()
+        audioPlayer?.play()
+    }
+    
+    @IBAction func playButtonPressed() {
+        if audioPlayer!.isPlaying {
+            audioPlayer?.pause()
+        } else {
+            audioPlayer?.play()
+        }
+    }
+    
+    @objc private func updateSlider() {
+        slider.value = Float(audioPlayer!.currentTime)
+    }
 }
