c1bdbe98b56dfc25e65e76ae2be283995ab30e61
Anatolii <AJR123123@proton>
2019-10-12 16:22:55 -0700

    create model

--- AudioPlayer/Models/TrackInfo.swift
+++ AudioPlayer/Models/TrackInfo.swift
@@ -10,5 +10,17 @@ import Foundation
 
 //Тут будет модель, содержащая информацию о треке
 struct TrackInfo {
-    
+    struct TrackInfo {
+        let artistNameLabel: String
+        let trackNameLabel: String
+        let coverImage: String
+        let userRating: UserRating?
+        let trackReleaseYearLabel: String
+    }
+
+    enum UserRating: String {
+        case dontLike = "I do not like 👎"
+        case notInteresting = "Not interesed ✊"
+        case like = "I like this 👍"
+    }
 }
